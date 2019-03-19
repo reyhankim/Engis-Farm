c65afe2af389410d740375cbd5e6d6a63c4c5764
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-03-19 20:43:53 +0700

    Update LinkedList.hpp

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,13 +1,17 @@
 #ifndef LINKEDLIST_HPP
 #define LINKEDLIST_HPP
 
-#include <vector>
 using namespace std;
 
 template <class T>
 class LinkedList{
 	private :
-		vector<T> list;
+		struct Node{
+			T info;
+			Node *next, *prev;
+		};
+		
+		Node *list;
 	
 	public :
 		LinkedList();
