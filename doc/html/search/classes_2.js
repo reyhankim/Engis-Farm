55513072d6d39a82e7f8c6e8a108bfd856ed68a9
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-03-20 09:55:54 +0700

    Folderization

--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['duck',['Duck',['../class_duck.html',1,'']]],
-  ['duckegg',['DuckEgg',['../class_duck_egg.html',1,'']]],
-  ['duckmeat',['DuckMeat',['../class_duck_meat.html',1,'']]]
+  ['duckegg',['DuckEgg',['../class_duck_egg.html',1,'']]]
 ];
--- doc/html/search/classes_5.js
+++ doc/html/search/classes_5.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['goat',['Goat',['../class_goat.html',1,'']]],
-  ['goatmeat',['GoatMeat',['../class_goat_meat.html',1,'']]],
   ['goatmilk',['GoatMilk',['../class_goat_milk.html',1,'']]],
   ['grassland',['Grassland',['../class_grassland.html',1,'']]]
 ];
