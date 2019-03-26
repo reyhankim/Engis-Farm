85a50125f0effe09813ffc911300b35f6ac389c7
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-03-26 12:34:02 +0700

    implementating Cell

---
+++ src/Cell.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "Cell.hpp"
+
+using namespace std;
+
+Cell::Cell(){
+    this->walkable = true;
+}
+
+bool Cell::isWalkable() const{
+    return this->walkable;
+}
+
+void Cell::setWalkable(bool input){
+    this->walkable = input;
+}
