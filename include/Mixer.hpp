c0a00744291b9d01113534466173a3a64b511bee
Stefanus Ardi Mulia <stefanus.ardimulia@gmail.com>
2019-04-05 05:47:06 +0700

    Change Mixer implementation to templates

--- include/Mixer.hpp
+++ include/Mixer.hpp
@@ -13,23 +13,12 @@
 
 // Class Mixer adalah blueprint dari objek turunan "Facility" yang dapat menghasilkan SideProduct dari gabungan FarmProduct
 class Mixer : public Facility {
-	private :
-		static int const row = 3;
-		Product **listSideProduct;
-		//struktur array
-		// | Side Product | Farm Product 1 | Farm Product 2 |
-	
 	public :
-		//CTOR
-		Mixer();
-		
-		//DTOR
-		~Mixer();
-		
 		//Render
 		char render() const override;
 		
 		//METHOD
-		SideProduct mix(FarmProduct bahan1, FarmProduct bahan2);
+		template <typename U, typename V>
+		SideProduct* mix(U bahan1, V bahan2);
 };
 #endif //ENGISFARM_MIXER_HPP
--- src/Mixer.cpp
+++ src/Mixer.cpp
@@ -11,48 +11,41 @@
 
 using namespace std;
 
-Mixer::Mixer(){
-    listSideProduct = new Product* [row];
-    for(int i=0;i<row;i++){
-        listSideProduct[i] = new Product [3];
-    }
-
-    listSideProduct[0][0] = new ChickenNugget();
-    listSideProduct[0][1] = new ChickenEgg();
-    listSideProduct[0][2] = new ChickenMeat();
-
-    listSideProduct[1][0] = new BeefRolade();
-    listSideProduct[1][1] = new CowMeat();
-    listSideProduct[1][2] = new ChickenEgg();
-
-    listSideProduct[2][0] = new BeefSteak();
-    listSideProduct[2][1] = new CowMeat();
-    listSideProduct[2][2] = new CowMilk();
-}
-
-Mixer::~Mixer(){
-    for(int i=0;i<row;i++){
-        delete [] listSideProduct[i];
-    }
-    delete [] listSideProduct;
-}
-
-char Mixer::render() const override{
+char Mixer::render() const{
     return 'X';
 }
 
-SideProduct Mixer::mix(FarmProduct bahan1, FarmProduct bahan2){
-    bool found = false;
-    for(int i=0;i<row;i++){
-        if(bahan1 = static_cast<FarmProduct>(listSideProduct[i][1]) != NULL){
-            if(bahan2 = static_cast<FarmProduct>(listSideProduct[i][2]) != NULL){
-                found = true;
-                return listSideProduct[i][0];
-            }
-        }
-    }
+template <>
+SideProduct* Mixer::mix<CowMeat, ChickenEgg>(CowMeat bahan1, ChickenEgg bahan2){
+    return new BeefRolade();
+}
 
-    if(!found){
-        return NULL;
-    }
+template <>
+SideProduct* Mixer::mix<ChickenEgg, CowMeat>(ChickenEgg bahan1, CowMeat bahan2){
+    return new BeefRolade();
+}
+
+template <>
+SideProduct* Mixer::mix<ChickenMeat, ChickenEgg>(ChickenMeat bahan1, ChickenEgg bahan2){
+    return new ChickenNugget();
+}
+
+template <>
+SideProduct* Mixer::mix<ChickenEgg, ChickenMeat>(ChickenEgg bahan1, ChickenMeat bahan2){
+    return new ChickenNugget();
+}
+
+template <>
+SideProduct* Mixer::mix<CowMeat, CowMilk>(CowMeat bahan1, CowMilk bahan2){
+    return new BeefSteak();
+}
+
+template <>
+SideProduct* Mixer::mix<CowMilk, CowMeat>(CowMilk bahan1, CowMeat bahan2){
+    return new BeefSteak();
+}
+
+template <typename U, typename V>
+SideProduct* Mixer::mix<U, V>(U bahan1, V bahan2){
+    return NULL;
 }
