f4938ed5f2b77a73f0b3590aef7877153f65ae01
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-04-05 02:03:57 +0700

    adding const chanceGrassed (land.hpp)

--- include/Land.hpp
+++ include/Land.hpp
@@ -14,6 +14,7 @@ class Land : public Cell {
 		bool grassed;           // Menyimpan boolean untuk mengetahui Land tersebut ditumbuhi rumput atau tidak
 		// menentukan secara acak apakah Land akan berrumput (dipanggil oleh CTOR)
 		void randomGrass();
+		int const chanceGrassed = 20;
 	
 	public :
 		//CTOR : Memaanggil randomGrassed()
